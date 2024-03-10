ddedd739a1a8981a0c6700ddd55484acb70e5ada
Cj Aguilar <rojayfire@gmail.com>
2024-03-10 23:17:13 +0800

    .

--- source/source.php
+++ source/source.php
@@ -101,7 +101,8 @@ if (isset($_POST['item_array'])) {
         $item_quantity = $item['item_quantity'];
         $item_price = $item['item_price'];
 
-        $insert_to_orderdetails = "INSERT INTO order_details (item_id, item_quantity, item_price) VALUES ('$item_id', '$item_quantity', '$item_price')";
+        $insert_to_orderdetails = "INSERT INTO order_details ((SELECT item_id FROM item WHERE = '$item_id'), item_quantity, item_price) 
+        VALUES ('$item_id', '$item_quantity', '$item_price')";
         $result = mysqli_query($conn, $insert_to_orderdetails);
     }
 }
